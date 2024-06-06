948c6142b8952aa2147424befbc3645e32aff799
varundoma <varunkumardoma35@gmail.com>
2024-06-06 10:10:50 +0530

    test

---
+++ input.py
@@ -0,0 +1,8 @@
+name = input("what is your name ?: ")
+age = int(input("how old are you? :  "))
+Height = float(input("how tall are you?: "))
+
+
+print("hello  "+name)
+print("your are  "+ str(age))
+print((Height))
